d2fc3accaff83d1711156cc84c89645e8ef3af0d
MoonKabir <moonkabir4@gmail.com>
2021-01-04 12:08:04 +0600

    weak2 contact page added and single page application added

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import Home from './HomeComponent';
 import Menu from './MenuComponent'
-import {DISHES} from '../shared/dishes'
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishDetailsComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
@@ -11,16 +15,25 @@ class Main extends Component {
 
     constructor(props){
         super(props);
+
         this.state = {
-        dishes: DISHES,
-        selectedDish: null
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        
+        // selectedDish: null
         };
     }
     render() {
 
         const HomePage = ()=> {
             return (
-                <Home/> 
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                /> 
             )
         }
 
@@ -32,6 +45,7 @@ class Main extends Component {
             <Route path="/home" component = {HomePage}/>
             {/* in the menu component have props and this value is passess through this way */}
             <Route exact path="/menu" component = {() => <Menu dishes={this.state.dishes} />}/>
+            <Route exact path="/contactus" component = {Contact}/>
             {/* <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> */}
             <Redirect to="/home"/>
         </Switch>
